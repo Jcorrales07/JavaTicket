4c013901a61f70e649265282a1e4078bfd6ba186
Jcorrales07 <corralesjoe07@gmail.com>
2022-01-31 21:13:39 -0600

    Avance de login, no me gusta Swing

---
+++ src/nvm/Main.java
@@ -0,0 +1,11 @@
+package nvm;
+
+import nvm.Login;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Login login = new Login(); 
+        login.setVisible(true);
+    }  
+}
