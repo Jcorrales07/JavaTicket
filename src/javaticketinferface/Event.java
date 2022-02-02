cce51367ac55d04bc762f55bbee8bb1bd75ad80b
Joe Corrales <corralesjoe07@gmail.com>
2022-02-01 20:12:10 -0600

    Parent class Event created

---
+++ src/javaticketinferface/Event.java
@@ -0,0 +1,12 @@
+package javaticketinferface;
+
+import java.util.Date;
+
+/** @author Joe Corrales */
+public class Event {
+    protected static int code;
+    protected String entTitle;
+    protected String entDescription;
+    protected Date date;
+    protected double rentAmount; 
+}
